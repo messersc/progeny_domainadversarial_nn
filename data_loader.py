e454dce1f02fecc703ddbaafa9168bbb3d7be470
Clemens Messerschmidt <messersc@users.noreply.github.com>
2021-07-16 08:52:58 +0200

    Data loader to process 2 sources + cbioportal z-scores to a common base

--- data_loader.py
+++ data_loader.py
@@ -1,20 +1,27 @@
 import numpy as np
 import pandas as pd
 import torch
-
-# config
-x1 = "/vol/home-vol3/wbi/messersc/dev/footprints/v1.0/Progeny_X.tsv"
-y1 = "/vol/home-vol3/wbi/messersc/dev/footprints/v1.0/Progeny_y.tsv"
-x2 = "/vol/home-vol3/wbi/messersc/dev/SPEED2_data_download/06_speed2_dataloader/Speed2_X.tsv"
-y2 = "/vol/home-vol3/wbi/messersc/dev/SPEED2_data_download/06_speed2_dataloader/Speed2_y.tsv"
-# end config
+import pickle
+from functools import reduce
 
 
 def read_tsv(path):
     return pd.read_csv(path, delimiter="\t", header=0, index_col=0)
 
 
-def dataloaders(X1, X2, Y1, Y2, top_mvg=4000):
+def get_class_weights(Y, names):
+    """
+    Return inverse of occurence of each class as weight.
+    Crash if a class has 0 occurences, as it should be filtered.
+    """
+    w = np.bincount(Y)
+    ws = sum(w)
+    assert len(names) == len(w)
+    d = dict(zip(names, ws / w))
+    return d
+
+
+def dataloaders(X1, X2, Y1, Y2, top_mvg=4000, cbiopickle=None):
     """load features and classes from two sources,
     subset both to their shared features and classes.
     Return three data loaders.
@@ -27,14 +34,16 @@ def dataloaders(X1, X2, Y1, Y2, top_mvg=4000):
     X2 = read_tsv(X2)
     Y2 = read_tsv(Y2)
 
-    # get intersection of features names and ordering
-    _, x1_ind, x2_ind = np.intersect1d(
-        X1.columns, X2.columns, assume_unique=True, return_indices=True
-    )
+    # also subset to cbio genes if present
+    if cbiopickle:
+        with open(cbiopickle, "rb") as f:
+            X3 = pickle.load(f)
+        common = reduce(np.intersect1d, (X1.columns, X2.columns, X3.columns))
+    else:
+        # get intersection of features names and ordering
+        common = np.intersect1d(X1.columns, X2.columns, assume_unique=True)
 
-    X1 = X1.iloc[:, x1_ind]
-    X2 = X2.iloc[:, x2_ind]
-    assert (X1.columns == X2.columns).all()
+    X1 = X1[common]
 
     # select the top n most variable features
     var = np.var(X1)
@@ -44,15 +53,15 @@ def dataloaders(X1, X2, Y1, Y2, top_mvg=4000):
     X1 = X1[var.index]
     X2 = X2[var.index]
     assert (X1.columns == X2.columns).all()
+    if cbiopickle:
+        X3 = X3[var.index]
+        assert (X1.columns == X3.columns).all()
 
     # get intersection of features names and ordering for Y too
     # remove classes that are not shared
-    _, x1_ind, x2_ind = np.intersect1d(
-        Y1.columns, Y2.columns, assume_unique=True, return_indices=True
-    )
-
-    Y1 = Y1.iloc[:, x1_ind]
-    Y2 = Y2.iloc[:, x2_ind]
+    common = np.intersect1d(Y1.columns, Y2.columns, assume_unique=True)
+    Y1 = Y1[common]
+    Y2 = Y2[common]
     assert (Y1.columns == Y2.columns).all()
 
     # remove all observations from Xs and Ys that do not belong to a class anymore
@@ -65,25 +74,68 @@ def dataloaders(X1, X2, Y1, Y2, top_mvg=4000):
     Y2 = Y2.iloc[keepers2, :]
     assert len(X2) == len(Y2)
 
-    # HACK
-    #return X1, Y1, X2, Y2
+    # ynames = Y1.columns
+    # Y1 = Y1.values.argmax(axis=1)
+    # Y2 = Y2.values.argmax(axis=1)
+    # w = get_class_weights(Y1, ynames)
 
-    Y1 = Y1.values.argmax(axis=1)
-    Y2 = Y2.values.argmax(axis=1)
+    # ds1 = torch.utils.data.TensorDataset(
+    #    torch.Tensor(X1.values),
+    #    torch.Tensor(Y1).type(torch.long),
+    # )
+
+    # ds2 = torch.utils.data.TensorDataset(
+    #    torch.Tensor(X2.values),
+    #    torch.Tensor(Y2).type(torch.long),
+    # )
+
+    ## HACK
+    ## For multi-label classification, one-hot enc classes are needed
+    ynames = Y1.columns
+    Y1_ = Y1.values.argmax(axis=1)
+    Y2_ = Y2.values.argmax(axis=1)
+    w = get_class_weights(Y1_, ynames)
+    Y1 = Y1.values
+    Y2 = Y2.values
+
+    import stratified_split
+
+    train_x, train_y, test_x, test_y = stratified_split.my_train_test_split(X1, Y1)
 
     ds1 = torch.utils.data.TensorDataset(
         torch.Tensor(X1.values),
-        torch.Tensor(Y1).type(torch.long),
+        torch.Tensor(Y1).type(torch.float32),
+    )
+
+    train = torch.utils.data.TensorDataset(
+        torch.Tensor(train_x.values),
+        torch.Tensor(train_y).type(torch.float32),
+    )
+    val = torch.utils.data.TensorDataset(
+        torch.Tensor(test_x.values),
+        torch.Tensor(test_y).type(torch.float32),
     )
 
     ds2 = torch.utils.data.TensorDataset(
         torch.Tensor(X2.values),
-        torch.Tensor(Y2).type(torch.long),
+        torch.Tensor(Y2).type(torch.float32),
     )
 
-    return ds1, ds2
+    if cbiopickle:
+        ds3 = torch.utils.data.TensorDataset(
+            torch.Tensor(X3.values),
+        )
+        # return ds1, ds2, w, (ds3, X3.index)
+        return ds1, train, val, ds2, w, (ds3, X3.index)
+
+    return ds1, train, val, ds2, w
 
 
 if __name__ == "__main__":
-    #ds1, ds2 = dataloaders(x1, x2, y1, y2)
-    x1, y1, x2, y2 = dataloaders(x1, x2, y1, y2)
+    # config
+    x1 = "/vol/home-vol3/wbi/messersc/dev/footprints/v1.0/Progeny_X.tsv"
+    y1 = "/vol/home-vol3/wbi/messersc/dev/footprints/v1.0/Progeny_y.tsv"
+    x2 = "/vol/home-vol3/wbi/messersc/dev/SPEED2_data_download/06_speed2_dataloader/Speed2_X.tsv"
+    y2 = "/vol/home-vol3/wbi/messersc/dev/SPEED2_data_download/06_speed2_dataloader/Speed2_y.tsv"
+    # end config
+    result = dataloaders(x1, x2, y1, y2)
